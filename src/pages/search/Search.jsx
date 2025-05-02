e29b27f3fe1804abfe5e1c34e6c732d70b18fbc5
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-05-02 10:26:50 -0500

    completndo pagina de busqueda de juegos

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useMemo } from 'react'
 import { useParams } from 'react-router-dom'
 import gamesScratch from '@/data/gamesScratch'
 import Nav from '@/components/nav/Nav'
@@ -6,23 +6,19 @@ import Games from '@/components/games/Games'
 
 function Search() {
 	const { grade } = useParams()
-	const gradeGames = useRef(null)
 
-	useEffect(() => {
+	const gradeGames = useMemo(() => {
 		const escapedGrade = grade.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 		const regex = new RegExp(`^${escapedGrade}(?:\\.\\d+)?$`)
-		gradeGames.current = gamesScratch.filter(game => 
+		return gamesScratch.filter(game => 
 			game.categories.some(category => regex.test(category))
 		)
-		console.log(gradeGames.current)
 	}, [grade])
 
 	return (
 		<>
 			<Nav />
-			{/*
-			<Games gamesScratch={gradeGames.current} />
-			*/}
+			<Games gamesScratch={gradeGames} />
 		</>
 	)
 }
