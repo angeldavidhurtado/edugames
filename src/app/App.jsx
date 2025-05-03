cf9a5e8d8516925eaf912f1807dd4f8cd816838c
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-05-03 14:16:05 -0500

    scroll to top

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,9 @@
 import { useRoutes } from 'react-router-dom'
+import useScrollToTop from '@/hooks/useScrollToTop'
 import routes from './routes'
 
 function App() {
+  useScrollToTop()
   const app = useRoutes(routes)
   return app
 }
