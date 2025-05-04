b6930ae23a34600d2e566d55b14027d6d85b35d9
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-05-04 09:21:03 -0500

    ajustando vista de juego

--- src/pages/play-game/PlayGame.jsx
+++ src/pages/play-game/PlayGame.jsx
@@ -5,17 +5,13 @@ import './PlayGame.css'
 
 function PlayGame() {
 	const { idGame } = useParams()
-	// https://scratch.mit.edu/projects/${idGame}/embed
 
 	return (
 		<>
 			<Nav />
 			<div className="game-content">
 				<div className="gradient-blue"></div>
-				<iframe
-					className="game"
-					src={`/edugames/256539458 - 6.3 7.3 - Sesión 3. Áreas y Perímetros.html`}
-				></iframe>
+				<iframe className="game" src={`/edugames/${idGame}.html`}></iframe>
 				<div className="gradient-red"></div>
 			</div>
 			<Games />
